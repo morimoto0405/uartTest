469ccd021668928b49ec528ef35e261c98f937c1
morimoto <morimoto@kotera-ss.jp>
2025-09-05 16:44:30 +0900

    初めてのコミッ

--- power-check.sh
+++ power-check.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-PIN=4
+set -e
 
-#GPIOエクスポート
-if [ ! -d /sys/class/gpio/gpio$PIN ]; then
-    echo "$PIN" > /sys/class/gpio/export
+#1 gpiod　のインストール
+if ! command -v gpiomon & > /dev/null; then
+    echo "gpiod not found. Installing..."
+    sudo apt update
+    sudo apt install -y gpiod
+else
+    echo "gpiod already installed."
 fi
-echo "in" > /sys/class/gpio/gpio$PIN/direction
-echo "Shutdown monitor started..."
 
-#無限ループで監視
-while true; do
-    VALUE=$(cat /sys/class/gpio/gpio$PIN/value)
-    if [ "$VALUE" -eq 1 ]; then
-        echo "Power loss detected! Shutting down..."
-        sudo shutdown -h now
-        break
-    fi
-    sleep 0.5
+#2 gpioを監視する準備
+PIN=4
+CHIP=gpiochip0
+
+#GPIO4を出力モードに設定し、LOWに
+echo "=== Setting GPIO$PIN mode ==="
+gpioset --mode=timebased $CHIP $PIN=0
+
+#GPIO4のエッジ監視
+echo "=== Monitoring GPIO$PIN for power loss ==="
+echo "Press Ctrl+C to exit"
+
+gpiomon -t 100 $CHIP $PIN | while read line; do
+    echo "Power loss detected!"
+    sudo shutdown -h now
 done
